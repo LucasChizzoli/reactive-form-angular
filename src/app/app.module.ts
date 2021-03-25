a5e0eba8ce1bbbe86659b3214be525ba1fc2faff
LucasChizzoli <chizzoli.lucas13@gmail.com>
2021-03-25 11:40:52 -0300

    Use login form component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { CardComponent } from './card/card.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    CardComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        ReactiveFormsModule,
+        FontAwesomeModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
